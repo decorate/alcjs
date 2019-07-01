85121df1843bda47a76b842854f03aa4bfeed853
mitu <mitu2687sp@gmail.com>
2019-07-01 13:34:57 +0900

    first

---
+++ src/components/Example.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1>Example!</h1>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    import Model from '../Model'
+
+    export default {
+        name: 'example',
+
+        data() {
+            return {}
+        },
+
+        async created() {
+            const m = new User({name: 'ok'})
+            console.log(m.getPostable())
+        },
+
+        methods: {
+        }
+    }
+
+    class User extends Model {
+        constructor(data) {
+            super()
+
+            this.fillable = ['id', 'name', 'password']
+            this.presents = ['password']
+
+            this.id = 0
+            this.name = ''
+            this.password = null
+
+            this.data = data
+        }
+    }
+</script>
